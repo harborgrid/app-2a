aae5c451c4cdcbbc30f203469c218c53c268ce7f
harborgrid-justin <justin.saadein@harborgrid.com>
2023-08-27 16:43:31 -0400

    a

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-const config = require('./config');         //
+const config = require('./config/engineConfig');         //
+
 const express = require('express');         //
 const bodyParser = require('body-parser');  //
 const app = express();                      //
